4a5d5378618d1e6710f24723a7e766cba0bfe575
vivekweb2013 <vivekweb2013@gmail.com>
2020-07-26 10:56:33 +0530

    Wrap error response under json key as error

--- src/main/java/com/wirehall/commandbuilder/dto/ErrorResponse.java
+++ src/main/java/com/wirehall/commandbuilder/dto/ErrorResponse.java
@@ -1,7 +1,11 @@
 package com.wirehall.commandbuilder.dto;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import java.util.List;
 
+@JsonTypeName("error")
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 public class ErrorResponse {
 
   private String message;
