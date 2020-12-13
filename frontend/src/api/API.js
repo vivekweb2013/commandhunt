43fc3b1270ba834552e2ee818011e69d63c3aa45
vivekweb2013 <vivekweb2013@gmail.com>
2020-12-13 08:30:43 +0530

    Assign api endpoint based on build type

--- frontend/src/api/API.js
+++ frontend/src/api/API.js
@@ -1,6 +1,6 @@
 import { getQueryParamsFromFilter } from '../Utils';
 
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+export const API_URL = process.env.NODE_ENV === 'development' ? "http://localhost:8080/api" : '/api';
 
 const getHeaders = () => {
     return {
