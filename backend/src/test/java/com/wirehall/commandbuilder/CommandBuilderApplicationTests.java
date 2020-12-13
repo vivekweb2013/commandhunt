3edc2a813099ee9e67955dead26ae5a3032689e4
vivekweb2013 <vivekweb2013@gmail.com>
2020-12-13 09:56:04 +0530

    Create dev profile to override dev props

--- backend/src/test/java/com/wirehall/commandbuilder/CommandBuilderApplicationTests.java
+++ backend/src/test/java/com/wirehall/commandbuilder/CommandBuilderApplicationTests.java
@@ -2,8 +2,10 @@ package com.wirehall.commandbuilder;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class CommandBuilderApplicationTests {
 
     @Test
